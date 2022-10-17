efa8ec34a254df7315f885e71f6ccd8ae0aee44f
Сергей Суханов <beent@yandex.ru>
2022-10-17 23:33:32 +0300

    headache

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <iostream>
-#include <filesystem>
 #include <windows.h>
 #include <stdio.h>
+#include <io.h>
 
-namespace fs = std::filesystem;
+using namespace std;
 
 HANDLE hStdin;
 DWORD fdwSaveOldMode;
@@ -13,8 +13,53 @@ DWORD fdwSaveOldMode;
 VOID KeyEventProc(KEY_EVENT_RECORD);
 VOID MouseEventProc(MOUSE_EVENT_RECORD);
 
+bool ShowDir(TCHAR path[]) {
+    //Показ содержимого текущей директории
+    _finddata64i32_t find;
+    TCHAR pathfind[MAX_PATH];
+    _tcscpy(pathfind, path);
+    _tcscat(pathfind, "\\*.*");
+    char info[MAX_PATH];
+    //Начало Поиска
+    int result = _findfirst64i32(pathfind, &find);
+    //Очистка экрана
+    system("cls");
+    int flag = result;
+    if (flag == -1) {
+        strcpy(info, "Такой Директории Нет");
+        //RussianMessage(info);
+        printf("%s\n", info);
+        return false;
+    }
+    while (flag != -1) {
+        if (strcmp(find.name, ".") && strcmp(find.name, "..")) {
+            //Проверяем Директория или Нет
+            find.attrib & _A_SUBDIR ? strcpy(info, " Каталог ") :
+                strcpy(info, " Файл ");
+            //RussianMessage(info);
+            //RussianMessage(find.name);
+            printf("%30s %10s\n", find.name, info);
+        }
+        //Продолжаем Поиск
+        flag = _findnext64i32(result, &find);
+    }
+    //ShowCurrentDir(path, info);
+    //Очищаем ресурсы, выделенные под поиск
+    _findclose(result);
+    return true;
+}
+
 int main(VOID)
 {
+    TCHAR path[MAX_PATH];
+    //path = "C:\\";
+    int getDir = GetCurrentDirectory(sizeof(path), path);
+    
+    //Показ содержимого текущей директории
+    ShowDir(path);
+
+    cout << "path: " << getDir << endl;
+
     DWORD cNumRead, fdwMode, i;
     INPUT_RECORD irInBuf[128];
     int counter = 0;
@@ -70,10 +115,6 @@ int main(VOID)
     // Restore input mode on exit.
 
     SetConsoleMode(hStdin, fdwSaveOldMode);
-
-    std::string path = "/path/to/directory";
-    for (const auto& entry : (std::filesystem::directory_iterator(path)))
-        std::cout << entry.path() << std::endl;
 
     return 0;
 }
